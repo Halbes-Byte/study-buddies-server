18b466c3697aaecd7cca44d2d327e513843fe2f6
Konstantin Pankratov <Konstantin.Pankratov@adorsys.com>
2024-12-08 18:49:09 +0100

    Add lombok notation to MeetingService

--- server/src/main/java/com/studybuddies/server/web/services/MeetingService.java
+++ server/src/main/java/com/studybuddies/server/web/services/MeetingService.java
@@ -4,19 +4,17 @@ import com.studybuddies.server.domain.MeetingEntity;
 import com.studybuddies.server.web.dto.MeetingCreationRequest;
 import com.studybuddies.server.web.mapper.MeetingMapper;
 import com.studybuddies.server.web.repositories.MeetingRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class MeetingService {
-    private final MeetingMapper meetingMapper;
-    private final MeetingRepository meetingRepository;
 
-    public MeetingService(MeetingMapper meetingMapper, MeetingRepository meetingRepository) {
-        this.meetingMapper = meetingMapper;
-        this.meetingRepository = meetingRepository;
-    }
+  private final MeetingMapper meetingMapper;
+  private final MeetingRepository meetingRepository;
 
-    public MeetingEntity saveMeeting(MeetingCreationRequest mcr) {
-        return meetingRepository.save(meetingMapper.MeetingCreationRequestToMeetingEntity(mcr));
-    }
+  public void saveMeeting(MeetingCreationRequest mcr) {
+    meetingRepository.save(meetingMapper.MeetingCreationRequestToMeetingEntity(mcr));
+  }
 }
