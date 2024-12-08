4c738e736f8a5dd6bd05c0b613f34d24d1142298
Konstantin Pankratov <Konstantin.Pankratov@adorsys.com>
2024-12-08 18:48:09 +0100

    Resort to lombok notation for constructor

--- server/src/main/java/com/studybuddies/server/web/MeetingController.java
+++ server/src/main/java/com/studybuddies/server/web/MeetingController.java
@@ -1,9 +1,9 @@
 package com.studybuddies.server.web;
 
 import com.studybuddies.server.web.dto.MeetingCreationRequest;
-import com.studybuddies.server.web.repositories.MeetingRepository;
 import com.studybuddies.server.web.services.MeetingService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,14 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/meeting")
 public class MeetingController {
-  private MeetingService meetingService;
-
-  public MeetingController(MeetingService meetingService) {
-    this.meetingService = meetingService;
-  }
+  private final MeetingService meetingService;
 
   @PostMapping
   public ResponseEntity<?> createMeeting(@Valid @RequestBody MeetingCreationRequest meetingCreationRequest) {
