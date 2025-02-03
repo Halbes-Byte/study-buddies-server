fd0ba0eba51fcb4a22101225a416840ca6b333a4
Konstantin Pankratov <Konstantin.Pankratov@adorsys.com>
2025-02-03 09:45:16 +0100

    Add tests for MeetingService

--- server/src/main/java/com/studybuddies/server/web/dto/MeetingChangeRequest.java
+++ server/src/main/java/com/studybuddies/server/web/dto/MeetingChangeRequest.java
@@ -1,5 +1,10 @@
 package com.studybuddies.server.web.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class MeetingChangeRequest {
     public String title;
     public String description;
