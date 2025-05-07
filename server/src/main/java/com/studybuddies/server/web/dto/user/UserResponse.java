40031f8c37adfd885eeef18ee7a73948eecac6d3
Konstantin Pankratov <Konstantin.Pankratov@adorsys.com>
2025-05-07 11:19:28 +0200

    Change dataflow

--- server/src/main/java/com/studybuddies/server/web/dto/user/UserResponse.java
+++ server/src/main/java/com/studybuddies/server/web/dto/user/UserResponse.java
@@ -1,6 +1,7 @@
 package com.studybuddies.server.web.dto.user;
 
 import com.studybuddies.server.web.dto.interfaces.Responses;
+import com.studybuddies.server.web.dto.module.ModuleResponse;
 import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,5 +14,5 @@ public class UserResponse implements Responses {
 
   String uuid;
   String username;
-  List<String> modules;
+  List<ModuleResponse> modules;
 }
